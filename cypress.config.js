654ff3a634407b1463f5fa29fbc8e9b0a1b3fdd4
Hansa <h.a.h.maduranga@mail.com>
2024-10-04 15:35:28 +0530

    Mocha add

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     "baseUrl": "https://www.booking.com",
     "chromeWebSecurity": false,
     "defaultCommandTimeout": 20000,
     "viewportWidth" : 1280,
     "viewportHeight" : 720,
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
       //require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
