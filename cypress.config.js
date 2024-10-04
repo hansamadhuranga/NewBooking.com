6b85782fda42a9a9daa732c5d4934841782ee747
Hansa <h.a.h.maduranga@mail.com>
2024-10-04 15:39:49 +0530

    add mocha

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  eporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     "baseUrl": "https://www.booking.com",
     "chromeWebSecurity": false,
