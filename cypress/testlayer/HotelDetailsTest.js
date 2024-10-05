d81e3c471cb28e7cdf58842dc5ec78b5ced4d0e6
Hansa <h.a.h.maduranga@mail.com>
2024-10-05 09:47:21 +0530

    updatehoteldetails page

--- cypress/testlayer/HotelDetailsTest.js
+++ cypress/testlayer/HotelDetailsTest.js
@@ -5,11 +5,19 @@ class HotelDetailTest{
         this.HotelDetailPage = new HotelDetailPage();
     }
 
+hotelinfro(){
+    this.HotelDetailPage.verifyHotelName(expectedHotelName);
+    this.HotelDetailPage.selectRoomByPrice(hotelPrice);
+    this.HotelDetailPage.clickReserve();
+    this.HotelDetailPage.fillPersonalDetails(firstName, lastName, email);
+    this.HotelDetailPage.clickNextFinalDetails();
+    this.HotelDetailPage.verifyFinalDetails(firstName, lastName);
+    this.HotelDetailPage.verifyCheckInOutDates(checkInDate, checkOutDate);
+}
 
 
 
 
 
 
-    
 }
